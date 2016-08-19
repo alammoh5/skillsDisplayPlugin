d29d21727232d3d5154a229b107be356196deb81
Osama Alam <mohammad.alam@live.com>
2016-08-19 14:51:38 -0400

    Widget to showoff skills

--- skills-plugin.php
+++ skills-plugin.php
@@ -38,6 +38,27 @@ function skillsdp_scripts() {
 add_action( 'wp_enqueue_scripts', 'skillsdp_scripts' );
 
 
+function fantastic_shortcode( $atts , $content=null ) {
+	extract( shortcode_atts(
+		   array(
+		   'color'=>'green',
+		   'size'=>'5px'
+		   ), $atts )
+		   );
+		   ?>
+		<style>
+		.color_me{
+			border: solid <?php echo $color ." ". $size ?>;
+		}
+		</style><?php
+	   return'<div class="color_me">'. get_the_post_thumbnail($content). '</div>';
+	   }
+	   
+	   add_shortcode( 'color_my_border', 'fantastic_shortcode' );
+
+
+
+
 // Create the Widget
 class chictonicSkillsDisplay extends WP_Widget {
 	
@@ -63,7 +84,29 @@ class chictonicSkillsDisplay extends WP_Widget {
 				<?php $my_query = new WP_Query( array( 'post_type' => 'tech_skill' ) ); ?>
 				<?php while ($my_query->have_posts()) : $my_query->the_post(); ?>
 				<li>
-				<?php get_template_part( 'template-parts/content-test', get_post_format() );?>
+					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+						<header class="entry-header">
+							<?php
+							if ( is_single() ) :
+								the_title( '<h1 class="entry-title">', '</h1>' );
+							else :
+								the_title( '<h2 class="entry-title">', '</h2>' );
+							endif; ?>
+						<?php echo do_shortcode('[color_my_border size="5px" color="gold"][/color_my_border]'); ?>
+						</header><!-- .entry-header -->
+						<div class="entry-content">
+							<?php
+								the_content( sprintf(
+									/* translators: %s: Name of current post. */
+									wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'chictonic' ), array( 'span' => array( 'class' => array() ) ) ),
+									the_title( '<span class="screen-reader-text">"', '"</span>', false )
+								) );
+								wp_link_pages( array(
+									'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'chictonic' ),
+									'after'  => '</div>',
+								) ); ?>
+						</div><!-- .entry-content -->
+					</article><!-- #post-## -->
 				</li>	
 				<?php endwhile; ?>
 				<?php wp_reset_query(); ?>
